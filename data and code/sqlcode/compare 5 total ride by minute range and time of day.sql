78eaad2ce8c1b4efdcf4f2b60fad3335b598611f
Binh Nguyen <103493942+binhnguyen5658@users.noreply.github.com>
2022-05-04 13:15:56 +0700

    Add files via upload

---
+++ data and code/sqlcode/compare 5 total ride by minute range and time of day.sql
@@ -0,0 +1,62 @@
+-- below 1 hour
+--- use minute by time of day
+WITH
+minute_range AS
+(
+	SELECT
+		ride_id,
+		member_casual,
+		datepart(hour, started_at) as started_at,
+		CASE	
+			WHEN datediff(minute, started_at, ended_at) BETWEEN 0 AND 15 THEN '0-15'
+			WHEN datediff(minute, started_at, ended_at) BETWEEN 16 AND 30 THEN '16-30'
+			WHEN datediff(minute, started_at, ended_at) BETWEEN 31 AND 45 THEN '31-45'
+			WHEN datediff(minute, started_at, ended_at) BETWEEN 46 AND 60 THEN '46-60'
+		END AS minute_range
+	FROM
+		previous_12_months_trip
+	WHERE
+		datediff(minute, started_at, ended_at) <= 60
+),
+member AS
+(
+	SELECT 
+		started_at,
+		minute_range,
+		COUNT(ride_id) as total_member_ride
+	FROM
+		minute_range
+	WHERE
+		member_casual = 'member'
+	GROUP BY
+		started_at,
+		minute_range
+),
+casual AS
+(
+	SELECT 
+		started_at,
+		minute_range,
+		COUNT(ride_id) as total_casual_ride
+	FROM
+		minute_range
+	WHERE
+		member_casual = 'casual'
+	GROUP BY
+		started_at,
+		minute_range
+)
+
+SELECT
+	member.started_at,
+	member.minute_range,
+	member.total_member_ride,
+	casual.total_casual_ride
+FROM
+	member
+JOIN 
+	casual ON member.started_at = casual.started_at 
+			AND member.minute_range = casual.minute_range
+ORDER BY
+	started_at ASC,
+	minute_range ASC;
